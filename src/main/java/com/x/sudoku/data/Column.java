bb6d6062a49b63816c4fb048a19ce95fcfe68bae
xiao <xchangyouren@sina.com>
2019-03-07 00:57:30 +0800

    [xiao] #N/A add more data object to support row/col compute

---
+++ src/main/java/com/x/sudoku/data/Column.java
@@ -0,0 +1,17 @@
+package com.x.sudoku.data;
+
+import com.google.common.collect.Sets;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Set;
+
+import static com.x.sudoku.SudokuGame.ALL_ELEMENTS;
+
+@Data
+public class Column {
+
+    private Set<SudokuNode> nodes;
+    @Builder.Default
+    private Set<Integer> possibleNumbers = Sets.newHashSet(ALL_ELEMENTS);
+}
